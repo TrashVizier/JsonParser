1500cbb92aefa121a15505572c814b36b402c961
TrashVizier <84637276+TrashVizier@users.noreply.github.com>
2021-06-07 20:38:04 +0200

    Update pastrugno.pl
    cambiamenti sperimentali su atomi e stringhe

--- pastrugno.pl
+++ pastrugno.pl
@@ -21,8 +21,9 @@
 %%% json/1
 % Il predicato è vero quando J è un Json
 
-json(J) :- 
-    rimuovi_newline(J, String),
+json(J) :-
+    atom_string(J, J1),             %%sperimentale
+    rimuovi_newline(J1, String),
     json_sup(String).
 
 json_sup(O) :- object(O), !.
@@ -78,10 +79,16 @@ value(I) :- array(I), !.
 %%% stringa/1
 % Il predicato è vero se S è una stringa 
                                                             % N.B. '"313"' è una stringa, "313" no
+% stringa(S) :-
+%     toglivirgolette(S, X),
+%     atom_string(X, Y),
+%     string(Y).
+
 stringa(S) :-
-    toglivirgolette(S, X),
-    atom_string(X, Y),
-    string(Y).
+    string(S), !.
+
+stringa(S) :-
+    atom(S), !.
 
 %%% num/1
 % Il predicato è vero se l'argomento è un numero
@@ -96,30 +103,31 @@ num(Input) :-
 
 %%% togligraffe/2
 togligraffe(I, I_senza_graffe) :- 
-    atom_concat('{', I_sx, I),
-    atom_concat(I_senza_graffe, '}', I_sx).
+    string_concat('{', I_sx, I),
+    string_concat(I_senza_graffe, '}', I_sx).
 
 %%% togliquadre/2
 togliquadre(I, I_senza_quadre) :- 
-    atom_concat('[', I_sx, I),
-    atom_concat(I_senza_quadre, ']', I_sx).
+    string_concat('[', I_sx, I),
+    string_concat(I_senza_quadre, ']', I_sx).
 
 %%% toglivirgolette/2
 toglivirgolette(I, I_pulito) :- 
-    atom_concat('"', I_sx, I),
-    atom_concat(I_pulito, '"', I_sx).
+    string_concat('"', I_sx, I),
+    string_concat(I_pulito, '"', I_sx).
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 json_parse(JSONString, Object) :-
     rimuovi_newline(JSONString, String),
+    % atom_string(String, String1),   %%aggiunta sperimentale
     parse_supp(String, Object).
 
 parse_supp(H, json_obj(Obj)) :- 
     object(H), !,
     togligraffe(H, H_senza_graffe),
-    member_breaker(H_senza_graffe, Lista_membri),
+    member_breaker(H_senza_graffe, Lista_membri),       %%Lista_membri è una lista di atomi
     maplist(spezza_pair, Lista_membri, Lista_s, Lista_v),
     maplist(check_value, Lista_v, Lista_v_aggiornata),
     maplist(incapsula_tonde, Lista_s, Lista_v_aggiornata, Obj).
@@ -139,9 +147,15 @@ attacca_array(Lista, json_array(Lista)).
 %%%check_value/2
 % Il predicato è vero quadno Il secondo argomento è il risultato del parsing del primo argomento
 
-check_value(N, N) :- num(N), !.
+check_value(N, N1) :- 
+    num(N),
+    !, 
+    atom_string(N, N1).
 
-check_value(S, S) :- stringa(S), !.
+check_value(S, S) :-
+    stringa(S),
+    !.
+    % atom_string(S, S1).
 
 check_value(V, X) :- parse_supp(V, X).
 
@@ -154,7 +168,7 @@ trim(String, Trim) :-
     reverse(Tsx, Tsxm),
     trim_sx(Tsxm, Tdxm),
     reverse(Tdxm, Tdx),
-    atom_chars(Trim, Tdx).
+    string_chars(Trim, Tdx).
 
 trim_sx([' ' | Tail], T) :-
     trim_sx(Tail, T).
@@ -167,7 +181,7 @@ trim_sx([H | Tail], T) :-
 %%% spezza_pair/3
 % Il predicato è vero quando il primo argomento è la coppia formata dal secondo e dal terzo argomento con un ':' nel mezzo (es. "a" : 3)
 
-spezza_pair(Coppia, St, Vt) :- 
+spezza_pair(Coppia, Ss, Vt) :- 
     atom_chars(Coppia, Chars),
     spezza_pair_sup(Chars, V_chars),
     append(S_chars_dp, V_chars, Chars),
@@ -175,7 +189,9 @@ spezza_pair(Coppia, St, Vt) :-
     atom_chars(S_dp, S_chars_dp),
     atom_concat(S, ':', S_dp),
     trim(S, St),
-    trim(V, Vt).
+    trim(V, Vt),
+    toglivirgolette(St, Ss).    %%sperimentale  %% funge per ora
+    % toglivirgolette(Vt, Vs).    %%sperimentale
 
 spezza_pair_sup([':' | Tail], Tail) :-!.
 
@@ -193,7 +209,7 @@ rimuovi_newline(String, Riga) :-
 concatena_lista([], X, X).
 
 concatena_lista([H | Tail], Precedente, String) :-
-    concat(Precedente, H, Successiva),  %%modificato atom_concat in concat
+    string_concat(Precedente, H, Successiva),  %%modificato atom_concat in string_concat
     concatena_lista(Tail, Successiva, String).
 
 %%% incapsula_tonde/3
@@ -205,6 +221,15 @@ incapsula_tonde(S, V, (S, V)).
 %     atom_concat(C1, ', ', C2),
 %     atom_concat(C2, V, C3),
 %     atom_concat(C3, ')', Capsula).
+
+
+%%supporto a caso WIP
+
+supporto_value(V, X) :-
+    atom_string(V1, V),
+    stringa(V1), !, 
+    toglivirgolette(Vt, Vs).
+
 
 %%% Funziona, ma mi da troppe opzioni e va ottimizzato
 %%% member_breaker/2
